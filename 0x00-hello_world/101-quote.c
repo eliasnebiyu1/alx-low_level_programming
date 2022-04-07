2a2d81cf87b904860c9b675b0c3f6109234e6d2c
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-07 09:47:17 -0700

    C Error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main -  responsible for starting the execution and termination
+ *
+ * Return: returns 1 (error)
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 /n");
+return (1);
+}
