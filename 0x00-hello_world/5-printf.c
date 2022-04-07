7e6eed3cd458b47b8dbd777c1e76276b45c06a90
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-07 04:51:52 -0700

    C printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - responsible for execution and termination of the program
  *
- * Return: returns 0 
+ * Return: returns 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\"");
 return (0);
 }
 
