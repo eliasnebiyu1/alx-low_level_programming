f5fe2ca8fa9dff8ba95b6dbc3bc0696df0bf95c6
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-07 03:48:56 -0700

    C Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-/* 
- * main Responsible for starting the execution and termination 
-*/
+/* main Responsible for starting the execution and termination */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
