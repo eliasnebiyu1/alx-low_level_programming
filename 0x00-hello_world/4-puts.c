9feb92d026e4781f08d6765f91bb44e49aa1d0dc
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-07 04:03:16 -0700

    C Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 /**
- *  main - Responsible for execution and termination
- *  
- *  Return - always returns 0
+ *  main - Responsible for execution and termination 
+ *  Return: always returns 0
  */
 int main(void)
 {
