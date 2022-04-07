b9d04c573d35afdfaf12a61b7128d8883ef6d24f
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-07 05:39:11 -0700

    C Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %lu \n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu \n", (unsigned long)sizeof(b));
+printf("Size of a char: %lu.\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu.\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu \n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu \n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu \n", (unsigned long)sizeof(e));
