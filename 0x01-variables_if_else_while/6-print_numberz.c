264f5035db4f6c2c8165248455242a7f7b8b6969
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-08 05:24:39 -0700

    Numbers putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Responsible for starting the execution and termination
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+
+for (int i = 0; i < 10 ; i++) 
+{
+putchar("%d", i);
+}
+putchar('\n');
+return (0);
+}
