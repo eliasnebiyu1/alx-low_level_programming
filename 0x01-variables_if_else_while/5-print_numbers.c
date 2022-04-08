095afc21939dc71c092e3cc6ed1b8f6c2443bb50
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-08 04:57:22 -0700

    Lower except e,q

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Responsible for starting the execution and termination
+ *
+ * Return: returns 0
+ */ 
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return(0);
+}
