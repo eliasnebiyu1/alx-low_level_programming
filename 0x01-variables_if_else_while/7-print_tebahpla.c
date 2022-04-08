d4525ff267baf2339f9855406ca38854c66ac63c
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-08 05:49:01 -0700

    Alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Responsible for starting the execution and termination
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 122; i > 96; i--)
+{
+putchar(i);
+}
+
+putchar('\n');
+return (0);
+}
