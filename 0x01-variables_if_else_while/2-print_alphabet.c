d0300338231f1d5a3d3cccc0aa7c9af634db87d7
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-08 03:38:22 -0700

    Lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Responsible for starting the execution and termination
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+char lca;
+lca = 'a';
+
+while(lca <= 'z')
+{
+putchar(lca);
+lca++;
+}
+return (0);
+}
