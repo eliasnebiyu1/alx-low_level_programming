ff50f587c3e85214b42a1402de5672d805f71663
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-08 02:44:08 -0700

    Random

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,19 +14,19 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n>0)
+if (n > 0)
 {
-printf("%d is positive", n);
+printf("%d is positive\n", n);
 }
 
-else if (n==0)
+else if (n == 0)
 {
-printf("%d is 0", n);
+printf("%d is zero\n", n);
 }
 
 else
 {
-printf("%d is negative", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
