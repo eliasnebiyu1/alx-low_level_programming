d2fb70baad03ce69ef9fcae52b7d028dde69f47b
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-12 08:09:02 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,20 @@
 #include "main.h"
 /**
- * main - Prints _putchar
- *
- * Return: returns 0
+ * print_alphabet_x10 - prints all lowercase alphabets 10 times 
  */
 
 void print_alphabet_x10(void)
 {
 int i = 0, j;
 
-while (i<10)
+while (i < 10)
 {
 
 while (j = 97; j < 123; j++)
 {
-	_putchar(i);
+_putchar(i);
 }
 
 }
 _putchar('\n');
-return (0);
 }
