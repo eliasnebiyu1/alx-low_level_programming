1f27cae2894fdf77478212616a1f9fdde388c9a6
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-12 09:44:41 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,11 +5,13 @@
  * @a: stores the first integer
  *
  * @b: stores the second integer
+ *
+ * Return: returns sum of 'a' and 'b'
  */
 
 int add(int a, int b)
 {
 int result;
 result = a + b;
-return result;
+return (result);
 }
