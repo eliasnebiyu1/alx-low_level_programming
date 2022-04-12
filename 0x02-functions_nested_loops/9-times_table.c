d78946fb781c1998ee94b2809f399bc750383b89
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-12 09:28:29 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ */
+
+void times_table(void)
+{
+int i, j, op;
+for (a = 0; a <= 9; a++)
+{
+_putchar(48);
+
+for (j = 0; j <= 9; j++)
+{
+op = i * j;
+_putchar(44);
+_putchar(32);
+
+if (op <= 9)
+{
+_putchar(32);
+_putchar(op + 48);
+}
+
+else
+{
+_putchar((op / 10) + 48);
+_putchar((op % 10) + 48);
+}
+
+}
+_putchar('\n');
+}
+
+}
