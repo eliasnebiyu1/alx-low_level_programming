456a1a8a4350ce14d7b80233a42de78af669f564
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-12 08:16:07 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@ void print_alphabet_x10(void)
 {
 int i = 0, j;
 
-while (i < 10)
+for (i < 10)
 {
 
-while (j = 97; j < 123; j++)
+for (j = 97; j < 123; j++)
 {
-_putchar(i);
+_putchar(j);
 }
 
 }
