dce3ff750996b1ad957a56e7b87669dd5ae8da29
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-13 11:19:15 -0700

    debug2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,17 +13,12 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-/**
- * while (i < 10)
- *
- * {
- *
- * Infinite loop - infinite
- *
- * putchar(i);
- *
- * }
- */
+/*
+while (i < 10)
+{
+putchar(i);
+}
+*/
 
 printf("Infinite loop avoided! \\o/\n");
 
