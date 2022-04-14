45a8ea89f8ad5aaee778ea958e5a7cbbc9398a44
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-14 12:12:03 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,22 @@ for (i = 2; i <= 100; i++)
 
 if (i % 3 == 0)
 {
-printf("Fizz");
+printf(" Fizz");
 }
 
 else if (i % 5 == 0)
 {
-printf("Buzz");
+printf(" Buzz");
 }
 
 else if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("FizzBuzz");
+printf(" FizzBuzz");
 }
 
 else
 {
-printf("%d", i);
+printf(" %d", i);
 }
 
 }
