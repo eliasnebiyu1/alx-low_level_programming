b83103a954aace9e6343c14e30422ffd1c9fe86c
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-14 06:33:48 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+int n;
+
+for (n = 48; n < 58; n++)
+{
+_putchar(n);
+}
+
+_putchar('\n');
+}
