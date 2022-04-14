b559a807b802ff76d62b4f42ed59a9594f4dc206
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-14 13:17:05 -0700

    print number adv

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,31 +8,18 @@
 
 void print_number(int n)
 {
-unsigned int i, j, count;
+unsigned int i = n;
 
 if (n < 0)
 {
-_putchar(45);
-i = n * -1;
+_putchar('-');
+i = -i;
 }
 
-else
+if ((i / 10) > 0)
 {
-i = n;
+print_number(i / 10);
 }
 
-j = i;
-count = 1;
-
-while (j > 9)
-{
-j /= 10;
-count *= 10;
-}
-
-for (; count >= 1; count /= 10)
-{
-_putchar(((i / count) % 10) + 48);
-}
-
+_putchar((i % 10) + '0');
 }
