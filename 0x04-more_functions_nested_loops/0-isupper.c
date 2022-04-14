1e1423e50a026fe580fb30e309e9b217a848b575
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-14 06:01:12 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * is_upper - tests whether values are uppercase or not
+ * 
+ * @c: stores values
+ *
+ * Return: returns 1 if value is uppercase elde returns 0
+ */
+
+int _isupper(int c)
+{
+
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
