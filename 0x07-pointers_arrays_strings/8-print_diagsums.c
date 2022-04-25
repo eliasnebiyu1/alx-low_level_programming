313b129f1a34f01017cabca25b538bca8ac51f3b
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-04-25 13:01:15 -0700

    crackme

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals of 
- * a square matrix of integers
+ * print_diagsums - prints the sum of the two diagonals 
  * @a: input pointer
  * @size: size of the matrix
  */
