8a582164fac49ff344f9581efed102215ee46545
Elias Nebiyu <eliasnebiyu022@gmail.com>
2022-05-09 10:20:40 -0700

    dog

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,17 +14,21 @@ if (n < 0)
 _putchar(45);
 m = n * -1;
 }
+
 else
 {
 m = n;
 }
+
 d = m;
 count = 1;
+
 while (d > 9)
 {
 d /= 10;
 count *= 10;
 }
+
 for (; count >= 1; count /= 10)
 {
 _putchar(((m / count) % 10) + 48);
